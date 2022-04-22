bdcbb92c9c7c8a4c48d096e395eee1251121daf5
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-22 00:15:10 -0300

    #23 - Stack, colocando background e mascara

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,51 +12,69 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 200,
+                width: 200,
+              ),
+              TextField(
+                onChanged: (String value) => email = value.toLowerCase(),
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: 'Email', border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                onChanged: (String value) => password = value,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Password', border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  if (email == 'admin@admin.com' && password == 'admin') {
+                    Navigator.pushReplacementNamed(context, '/home');
+                  } else {
+                    print('Login e senha errado');
+                  }
+                },
+                child: const Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 200,
-                  width: 200,
-                ),
-                TextField(
-                  onChanged: (String value) => email = value.toLowerCase(),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  onChanged: (String value) => password = value,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      labelText: 'Password', border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'admin@admin.com' && password == 'admin') {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    } else {
-                      print('Login e senha errado');
-                    }
-                  },
-                  child: const Text('Entrar'),
-                ),
-              ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+          _body(),
+        ],
       ),
     );
   }
